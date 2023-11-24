5f959248724606170a5076a475ce81fee6c41d28
МшкÑMirsaidovUmed <umedmirsaidov61@gmail.com>
2023-11-24 14:23:29 +0500

    fixed array_3

--- array/array_3/main.go
+++ array/array_3/main.go
@@ -3,38 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		numArr     int
-		biggestNum int
-	)
-	fmt.Scan(&numArr)
-	arr := make([]int, numArr)
-	for i := 0; i < numArr; i++ {
+	var sizeOfArray int
+	fmt.Scan(&sizeOfArray)
+	arr := make([]int, sizeOfArray)
+
+	// заполняем массив
+	for i := 0; i < sizeOfArray; i++ {
 		fmt.Scan(&arr[i])
-		if arr[i]%2 != 0 {
-			if i == 0 || arr[i] < biggestNum {
-				biggestNum = arr[i]
-			}
-		} else {
-			fmt.Println(0)
-			break
+	}
+
+	maxElement := arr[0]
+	for i := 1; i < sizeOfArray; i++ {
+		if arr[i] > maxElement {
+			maxElement = arr[i]
 		}
 	}
-	fmt.Println(biggestNum)
+	fmt.Println(maxElement)
 }
-
-//var n, biggestNum int
-//fmt.Scan(&n)
-//arr := make([]int, n)
-//for i := 0; i < n; i++ {
-//	fmt.Scan(&arr[i])
-//}
-//for i := 0; i < n-1; i++ {
-//	if arr[i] < arr[i+1] {
-//		biggestNum = arr[i+1]
-//	} else {
-//		biggestNum = arr[i]
-//		break
-//	}
-//}
-//fmt.Println(biggestNum)
